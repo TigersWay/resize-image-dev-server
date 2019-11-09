9c2fc08dd353d862ac23be8c32c45d071620f473
Ben Michaud <ben@tigersway.net>
2019-11-09 16:01:41 +0700

    Better transformations + Netlify mode

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # resize-image-dev-server
-Little standalone server for resizing/cropping local images (fake CDN feature) 
+Little standalone server for resizing/cropping local images (fake CDN feature)
+
+Default:
+
+http://localhost:[port|3000]/images/path/to/the/image.jpg?h=200&w=200
+
+Netlify:
+
+http://localhost:[port|3000]/images/netlify/path/to/the/image.jpg?nf_resize=fit&w=300
+http://localhost:[port|3000]/images/netlify/path/to/the/image.jpg?nf_resize=smartcrop&w=300&h=400
--- index.js
+++ index.js
@@ -12,19 +12,53 @@ const args = require('command-line-args')([
 ]);
 
 
-server.get('/image/:image(*)', function (req, res) {
-  const imagePath = `${args.path}/${req.params.image}`;
-  console.log(imagePath);
+const imageStream = (filename) => {
+  return fs.createReadStream(`${args.path}/${filename}`);
+}
 
-  const readableStream = fs.createReadStream(imagePath);
+server.get('/image/netlify/:image(*.(jpg|jpeg|png)$)', function (req, res) {
+  console.log(`Netlify: ${req.params.image}`);
 
-  options = {};
+  let valid = true;
+  let options = {};
+  switch (req.query.nf_resize) {
+    case 'fit':
+      options.fit = sharp.fit.inside;
+      break;
+    case 'smartcrop':
+      options.fit = sharp.fit.cover;
+      options.position = sharp.strategy.entropy;
+      break;
+    default:
+      valid = false;    // 'nf_resize' is required
+  }
   options.width = req.query.w ? parseInt(req.query.w) : null;
   options.height = req.query.h ? parseInt(req.query.h) : null;
 
-  const transformer = sharp().resize(options);
+  if (valid) {
+    imageStream(req.params.image)
+      .pipe(sharp().resize(options))
+      .pipe(res);
 
-  readableStream.pipe(transformer).pipe(res);
+  } else {
+    imageStream(req.params.image)
+      .pipe(res);
+  }
+});
+
+server.get('/image/:image(*.(jpg|jpeg|png)$)', function (req, res) {
+  console.log(`Default: ${req.params.image}`);
+
+  let options = {
+    fit: sharp.fit.cover,
+    position: sharp.strategy.entropy
+  };
+  options.width = req.query.w ? parseInt(req.query.w) : null;
+  options.height = req.query.h ? parseInt(req.query.h) : null;
+
+  imageStream(req.params.image)
+    .pipe(sharp().resize(options))
+    .pipe(res);
 });
 
 
